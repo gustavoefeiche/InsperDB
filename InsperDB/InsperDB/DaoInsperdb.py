0d3cb6d9f5cc6232fdd435eb9ac7fbae0bdd1ee2
GabrielValeRios <contato_rios@hotmail.com>
2017-09-01 09:43:06 -0230

    business layer arrumado

--- InsperDB/InsperDB/DaoInsperdb.py
+++ InsperDB/InsperDB/DaoInsperdb.py
@@ -1,12 +1,33 @@
 # The User Data Access Object handles all interactions with the User table.
 class DaoInsperdb:
        
-    def __init__(self, db):
-        self.db = db
+    def __init__(self, app):
+
+        self.app = app
+
+    def configApp(self):
+
+        self.app.config['MYSQL_HOST'] = 'localhost'
+        self.app.config['MYSQL_USER'] = 'root'
+        self.app.config['MYSQL_PASSWORD'] = 'mariopalestra1'
+        self.app.config['MYSQL_DB'] = 'insperdb'
+
+        return "Done"
+
+    def initializeCursor(self):
+        self.db = self.mysql.connection.cursor()
+        return "done"
+
+    def initializeSQL(self, MySQL):
+
+        self.mysql = MySQL(self.app)
+
+        return "done"
 
     def insertValuesPerson(self, person_email, person_name):
    
         self.db.execute('''INSERT INTO Person (Person_email, Person_name) VALUES (%s, %s)''', (person_email, person_name))
         rv = self.db.fetchall()
+        self.mysql.connection.commit()
         return str(rv)
       
--- InsperDB/InsperDB/flaskRios.py
+++ InsperDB/InsperDB/flaskRios.py
@@ -5,26 +5,21 @@ import json
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'your password'
-app.config['MYSQL_DB'] = 'insperdb'
-
-mysql = MySQL(app)
+obj = DaoInsperdb(app)
+obj.configApp()
+obj.initializeSQL(MySQL)
 
 @app.route('/add', methods = ['GET', 'POST'])
 def add():
 
 	if request.method == 'POST':
 
+		obj.initializeCursor()
+
 		js = json.loads(request.get_json(force = True))
 		email = js['email']
 		pw =  js['password']
-
-		db = mysql.connection.cursor()
-		obj = DaoInsperdb(db)
 		obj.insertValuesPerson(email, pw)
-		mysql.connection.commit()
 		return "Done"
 
 if __name__ == '__main__':
