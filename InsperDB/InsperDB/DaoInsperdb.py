94a733c2af411147c1d14df0a493bd1a7cecbf44
GabrielValeRios <contato_rios@hotmail.com>
2017-09-06 11:28:20 -0230

    file.txt

--- InsperDB/InsperDB/DaoInsperdb.py
+++ InsperDB/InsperDB/DaoInsperdb.py
@@ -12,12 +12,12 @@ class DaoInsperdb:
         for i in file:
 
             lines.append(i)
-        print(lines)
-            
-        self.app.config['MYSQL_HOST'] = '%s',((lines[0])[:-1])
-        self.app.config['MYSQL_USER'] = '%s',((lines[1])[:-1])
-        self.app.config['MYSQL_PASSWORD'] = '%s',((lines[2])[:-1])
-        self.app.config['MYSQL_DB'] = '%s',(lines[0])
+        
+        
+        self.app.config['MYSQL_HOST'] = lines[0][:-1]
+        self.app.config['MYSQL_USER'] = lines[1][:-1]
+        self.app.config['MYSQL_PASSWORD'] = lines[2][:-1]
+        self.app.config['MYSQL_DB'] = lines[3]
 
         return "done"
 
